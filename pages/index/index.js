f96b5945646bd43ff1981f67a958ded877b1db30
27354 <15179884526@163.com>
2019-08-13 15:38:22 +0800

    主页

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,27 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    grids: [
+      { img: '/source/images/iocn_01.png', title: '商城' },
+      { img: '/source/images/iocn_02.png', title: '订单' },
+      { img: '/source/images/iocn_03.png', title: '财富' },
+      { img: '/source/images/iocn_04.png', title: '人员' },
+      { img: '/source/images/iocn_05.png', title: '素材' },
+      { img: '/source/images/iocn_06.png', title: '我的' }],
+    imgUrls: [
+      'https://www.baidu.com/img/bd_logo1.png',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    mesinfo: [
+      { img: '/source/images/iocn_01.png', title: '通知1' },
+      { img: '/source/images/iocn_01.png', title: '通知2' },
+      { img: '/source/images/iocn_01.png', title: '通知3' },
+
+    ],
+
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    
   }
 })
